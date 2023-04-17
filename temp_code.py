d5d6bc30bcfefc3f235ded4a08febe444aba0ee8
1531724247@qq.com <1531724247@qq.com>
2023-04-17 10:28:20 +0800

    优化代码

--- temp_code.py
+++ temp_code.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandas_ta as ta
 import plotly.graph_objs as go
 import plotly.io as pio
 import requests
@@ -30,6 +31,18 @@ for val_list in kline_infos:
         val_list = val_list[:6]
         klines.append(val_list)
     else:
-        pass
+        pass# 用pandas_ta计算klines的sma指标
+
 
 df = get_data_frame(kline_list=klines)
+# 定义函数识别出波段高低点中的动量转换
+def momentum_conversion(hl_point_list):
+    momentum_list = []
+    for i in range(1, len(hl_point_list)):
+        if not pd.isna(hl_point_list[i]) and not pd.isna(hl_point_list[i-1]):
+            momentum_list.append(hl_point_list[i] - hl_point_list[i-1])
+        else:
+            momentum_list.append(pd.NaT)
+    return momentum_list
+
+hl_point_list = [nan, nan, nan, nan, nan, 0.2714, nan, 0.2656, nan, nan, nan, nan, nan, 0.2712, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.2604, nan, nan, nan, nan, 0.2651, nan, 0.2484, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.2947, nan, nan, nan, nan, nan, nan, 0.279, nan, nan, nan, nan, nan, 0.2925, nan, nan, 0.2833, nan, 0.3175, nan, 0.306, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.595, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.4546, nan, 0.494, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.4515, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.6538, nan, nan, nan, 0.596, nan, nan, nan, nan, 0.69, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.5913, nan, 0.6182, nan, nan, nan, nan, nan, 0.5898, nan, nan, nan, nan, nan, nan, 0.746, nan, nan, 0.653, nan, nan, nan, nan, 0.71, nan, nan, nan, nan, nan, nan, nan, 0.6601, nan, nan, nan, nan, 0.7347, nan, nan, nan, nan, 0.6471, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.8548, nan, nan, 0.8006, nan, nan, nan, nan, 0.9392, nan, nan, nan, nan, nan, nan, 0.7752, nan, nan, nan, nan, 0.843, nan, nan, nan, nan, nan, nan, nan, 0.787, nan, nan, nan, 0.828, 0.7853, nan, nan, nan, 0.8394, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.703, nan, nan, 0.7689, nan, nan, nan, nan, nan, 0.7141, nan, nan, nan, nan, 0.7752, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.7043, nan, nan, 0.732, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.6713, nan, nan, nan, 0.6866, nan, 0.6657, nan, nan, nan, nan, nan, 0.7269, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.626, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0.6319, nan, nan, 0.61, nan, nan, nan, nan, nan, nan, nan, nan, 0.6472, nan, nan, nan, nan, 0.6003, nan, nan, nan, nan, nan, 0.637, nan, nan]
